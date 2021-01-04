5ea4edddb5a5f1aef911c9249e68d0b916fa641f
kEOonlly_ <kEOonlly_@users.noreply.github.com>
2021-01-04 22:50:50 +0700

    Add project files.

---
+++ dashboardguna/DashBFrm.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DashBFrm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(DashBFrm))
+        Me.Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Guna2ShadowForm1 = New Guna.UI2.WinForms.Guna2ShadowForm(Me.components)
+        Me.Guna2DragControl1 = New Guna.UI2.WinForms.Guna2DragControl(Me.components)
+        Me.Guna2CustomGradientPanel1 = New Guna.UI2.WinForms.Guna2CustomGradientPanel()
+        Me.Guna2Button3 = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Button2 = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
+        Me.bnbtnhome = New Guna.UI2.WinForms.Guna2Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Guna2DragControl2 = New Guna.UI2.WinForms.Guna2DragControl(Me.components)
+        Me.Guna2CustomGradientPanel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Guna2Elipse1
+        '
+        Me.Guna2Elipse1.BorderRadius = 30
+        Me.Guna2Elipse1.TargetControl = Me
+        '
+        'Guna2ShadowForm1
+        '
+        Me.Guna2ShadowForm1.BorderRadius = 30
+        Me.Guna2ShadowForm1.TargetForm = Me
+        '
+        'Guna2DragControl1
+        '
+        Me.Guna2DragControl1.ContainerControl = Me
+        Me.Guna2DragControl1.TargetControl = Me
+        '
+        'Guna2CustomGradientPanel1
+        '
+        Me.Guna2CustomGradientPanel1.BorderRadius = 18
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.Guna2Button3)
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.Guna2Button2)
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.Guna2Button1)
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.bnbtnhome)
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.PictureBox1)
+        Me.Guna2CustomGradientPanel1.FillColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(143, Byte), Integer), CType(CType(179, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor4 = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.Location = New System.Drawing.Point(12, 9)
+        Me.Guna2CustomGradientPanel1.Name = "Guna2CustomGradientPanel1"
+        Me.Guna2CustomGradientPanel1.ShadowDecoration.Parent = Me.Guna2CustomGradientPanel1
+        Me.Guna2CustomGradientPanel1.Size = New System.Drawing.Size(85, 438)
+        Me.Guna2CustomGradientPanel1.TabIndex = 0
+        '
+        'Guna2Button3
+        '
+        Me.Guna2Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2Button3.BorderRadius = 24
+        Me.Guna2Button3.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
+        Me.Guna2Button3.CheckedState.BorderColor = System.Drawing.Color.White
+        Me.Guna2Button3.CheckedState.FillColor = System.Drawing.Color.White
+        Me.Guna2Button3.CheckedState.Image = CType(resources.GetObject("Guna2Button3.CheckedState.Image"), System.Drawing.Image)
+        Me.Guna2Button3.CheckedState.Parent = Me.Guna2Button3
+        Me.Guna2Button3.CustomImages.Parent = Me.Guna2Button3
+        Me.Guna2Button3.FillColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.Guna2Button3.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2Button3.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button3.HoverState.Parent = Me.Guna2Button3
+        Me.Guna2Button3.Image = CType(resources.GetObject("Guna2Button3.Image"), System.Drawing.Image)
+        Me.Guna2Button3.Location = New System.Drawing.Point(25, 348)
+        Me.Guna2Button3.Name = "Guna2Button3"
+        Me.Guna2Button3.ShadowDecoration.Parent = Me.Guna2Button3
+        Me.Guna2Button3.Size = New System.Drawing.Size(60, 50)
+        Me.Guna2Button3.TabIndex = 3
+        Me.Guna2Button3.UseTransparentBackground = True
+        '
+        'Guna2Button2
+        '
+        Me.Guna2Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2Button2.BorderRadius = 24
+        Me.Guna2Button2.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
+        Me.Guna2Button2.CheckedState.BorderColor = System.Drawing.Color.White
+        Me.Guna2Button2.CheckedState.FillColor = System.Drawing.Color.White
+        Me.Guna2Button2.CheckedState.Image = CType(resources.GetObject("Guna2Button2.CheckedState.Image"), System.Drawing.Image)
+        Me.Guna2Button2.CheckedState.Parent = Me.Guna2Button2
+        Me.Guna2Button2.CustomImages.Parent = Me.Guna2Button2
+        Me.Guna2Button2.FillColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.Guna2Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2Button2.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button2.HoverState.Parent = Me.Guna2Button2
+        Me.Guna2Button2.Image = CType(resources.GetObject("Guna2Button2.Image"), System.Drawing.Image)
+        Me.Guna2Button2.Location = New System.Drawing.Point(25, 276)
+        Me.Guna2Button2.Name = "Guna2Button2"
+        Me.Guna2Button2.ShadowDecoration.Parent = Me.Guna2Button2
+        Me.Guna2Button2.Size = New System.Drawing.Size(60, 50)
+        Me.Guna2Button2.TabIndex = 2
+        Me.Guna2Button2.UseTransparentBackground = True
+        '
+        'Guna2Button1
+        '
+        Me.Guna2Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2Button1.BorderRadius = 24
+        Me.Guna2Button1.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
+        Me.Guna2Button1.CheckedState.BorderColor = System.Drawing.Color.White
+        Me.Guna2Button1.CheckedState.FillColor = System.Drawing.Color.White
+        Me.Guna2Button1.CheckedState.Image = CType(resources.GetObject("Guna2Button1.CheckedState.Image"), System.Drawing.Image)
+        Me.Guna2Button1.CheckedState.Parent = Me.Guna2Button1
+        Me.Guna2Button1.CustomImages.Parent = Me.Guna2Button1
+        Me.Guna2Button1.FillColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.Guna2Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2Button1.ForeColor = System.Drawing.Color.White
+        Me.Guna2Button1.HoverState.Parent = Me.Guna2Button1
+        Me.Guna2Button1.Image = CType(resources.GetObject("Guna2Button1.Image"), System.Drawing.Image)
+        Me.Guna2Button1.Location = New System.Drawing.Point(25, 203)
+        Me.Guna2Button1.Name = "Guna2Button1"
+        Me.Guna2Button1.ShadowDecoration.Parent = Me.Guna2Button1
+        Me.Guna2Button1.Size = New System.Drawing.Size(60, 50)
+        Me.Guna2Button1.TabIndex = 1
+        Me.Guna2Button1.UseTransparentBackground = True
+        '
+        'bnbtnhome
+        '
+        Me.bnbtnhome.BackColor = System.Drawing.Color.Transparent
+        Me.bnbtnhome.BorderRadius = 24
+        Me.bnbtnhome.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton
+        Me.bnbtnhome.Checked = True
+        Me.bnbtnhome.CheckedState.BorderColor = System.Drawing.Color.White
+        Me.bnbtnhome.CheckedState.FillColor = System.Drawing.Color.White
+        Me.bnbtnhome.CheckedState.Image = CType(resources.GetObject("bnbtnhome.CheckedState.Image"), System.Drawing.Image)
+        Me.bnbtnhome.CheckedState.Parent = Me.bnbtnhome
+        Me.bnbtnhome.CustomImages.Parent = Me.bnbtnhome
+        Me.bnbtnhome.FillColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.bnbtnhome.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.bnbtnhome.ForeColor = System.Drawing.Color.White
+        Me.bnbtnhome.HoverState.Parent = Me.bnbtnhome
+        Me.bnbtnhome.Image = CType(resources.GetObject("bnbtnhome.Image"), System.Drawing.Image)
+        Me.bnbtnhome.Location = New System.Drawing.Point(25, 120)
+        Me.bnbtnhome.Name = "bnbtnhome"
+        Me.bnbtnhome.ShadowDecoration.Parent = Me.bnbtnhome
+        Me.bnbtnhome.Size = New System.Drawing.Size(60, 50)
+        Me.bnbtnhome.TabIndex = 0
+        Me.bnbtnhome.UseTransparentBackground = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(40, 94)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(65, 103)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.White
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(97, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(703, 457)
+        Me.Panel2.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Guna2CustomGradientPanel1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(97, 457)
+        Me.Panel1.TabIndex = 0
+        '
+        'Guna2DragControl2
+        '
+        Me.Guna2DragControl2.ContainerControl = Me
+        Me.Guna2DragControl2.TargetControl = Me.Panel2
+        '
+        'DashBFrm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 457)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.Name = "DashBFrm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Form1"
+        Me.Guna2CustomGradientPanel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Guna2ShadowForm1 As Guna.UI2.WinForms.Guna2ShadowForm
+    Friend WithEvents Guna2DragControl1 As Guna.UI2.WinForms.Guna2DragControl
+    Friend WithEvents Guna2CustomGradientPanel1 As Guna.UI2.WinForms.Guna2CustomGradientPanel
+    Friend WithEvents Guna2Button3 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Button2 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents bnbtnhome As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Guna2DragControl2 As Guna.UI2.WinForms.Guna2DragControl
+End Class
---
+++ dashboardguna/DashBFrm.vb
@@ -0,0 +1,11 @@
+﻿
+Public Class DashBFrm
+    Private Sub movIMG(sender As Object)
+        Dim b As Guna.UI2.WinForms.Guna2Button = CType(sender, Guna.UI2.WinForms.Guna2Button)
+        PictureBox1.Location = New Point(b.Location.X + 15, b.Location.Y - 26)
+        PictureBox1.SendToBack()
+    End Sub
+    Private Sub bnbtnhome_CheckedChanged(sender As Object, e As EventArgs) Handles Guna2Button3.CheckedChanged, Guna2Button2.CheckedChanged, Guna2Button1.CheckedChanged, bnbtnhome.CheckedChanged
+        movIMG(sender)
+    End Sub
+End Class
